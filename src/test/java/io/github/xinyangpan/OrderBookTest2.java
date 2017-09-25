6a7b27be4f8426c7d526e48176cd1cb194552597
Pan, Xinyang <seanpan2008@gmail.com>
2017-09-25 12:40:01 +0800

    1

--- src/test/java/io/github/xinyangpan/OrderBookTest2.java
+++ src/test/java/io/github/xinyangpan/OrderBookTest2.java
@@ -10,7 +10,7 @@ import io.github.xinyangpan.ella.core.bo.Side;
 public class OrderBookTest2 {
 
 	@Test
-	public void limitSell500() {
+	public void several() {
 		OrderBook orderBook = NoneMatchBook.bookSample1();
 		System.out.println(orderBook.toOrderBoardStr());
 		System.out.println(orderBook.placeOrder(newOrder(Side.SELL, 500, 120.01)));
--- src/test/java/io/github/xinyangpan/OrderUtils.java
+++ src/test/java/io/github/xinyangpan/OrderUtils.java
@@ -1,14 +1,17 @@
 package io.github.xinyangpan;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import io.github.xinyangpan.ella.core.bo.Order;
 import io.github.xinyangpan.ella.core.bo.OrderType;
 import io.github.xinyangpan.ella.core.bo.Side;
 
 public class OrderUtils {
+	private static AtomicLong id = new AtomicLong(1);
 
 	public static Order newOrder(Side side, long quantity, double price) {
 		Order order = new Order();
-		order.setId(1);
+		order.setId(id.getAndIncrement());
 		order.setOrderType(OrderType.LIMIT);
 		order.setQuantity(quantity);
 		order.setFilledQuantity(0);
@@ -21,7 +24,7 @@ public class OrderUtils {
 
 	public static Order newOrder(Side side, long quantity) {
 		Order order = new Order();
-		order.setId(1);
+		order.setId(id.getAndIncrement());
 		order.setOrderType(OrderType.MARKET);
 		order.setQuantity(quantity);
 		order.setFilledQuantity(0);
